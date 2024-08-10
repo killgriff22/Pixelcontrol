3aace26b47f3693bdd6779bbc34e1ac125741621
killgriff22 <kilgrif22033@gmail.com>
2024-08-10 17:38:58 -0400

    force chase to run the main do to the end, and clear before making changes to the pixels

--- main.py
+++ main.py
@@ -54,11 +54,12 @@ def rainbow_cycle(wait,j):
     time.sleep(wait)
 
 def chase(pos,front_tail_width=3,back_tail_width=3):
-    pixels[pos] = COLOR
+    pixels.fill((0, 0, 0))
     for i in range(1,front_tail_width+1):
         pixels[pos-i] = impose_color(COLOR,1/i)
     for i in range(1,back_tail_width+1):
-        pixels[pos+i] = impose_color(COLOR,1/i)
+        pixels[pos+i if pos+i < num_pixels else -1] = impose_color(COLOR,1/i)
+    pixels[pos] = COLOR
     pixels.show()
     
 
