b06e6773b22ea932c721017c67d547f19987b081
killgriff22 <kilgrif22033@gmail.com>
2024-08-11 17:19:07 -0400

    begin rewrite, also doing some debugging in hopes of getting ti working as is

--- main.py
+++ main.py
@@ -14,30 +14,18 @@ import time
 import board
 import neopixel
 import threading
-
-
+app = flask.Flask(__name__)
 def impose_color(t,value):
     return tuple(int((ele1 * value)//1) for ele1 in t)
-
-
-COLOR = (153, 103, 52)
-direction = 1
-speed = 1
-# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D18
-
-# The number of NeoPixels
 num_pixels = 60
-app = flask.Flask(__name__)
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.RGB
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=1.0, auto_write=False, pixel_order=ORDER
 )
 pixels.fill((0, 0, 0))
 pixels.show()
+COLOR = (153, 103, 52)
 
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
@@ -136,7 +124,7 @@ def mainloop():
             case "bounce":
                 if pattern[1] > num_pixels-1 or pattern[1] < 0:
                     pattern[5] *= -1
-        print(speed, pattern[1], direction)
+        print(pattern[4], pattern[1], pattern[5])
         time.sleep(0.1)
 mainthread = threading.Thread(target=mainloop)
 mainthread.start()
