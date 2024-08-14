c3290cfeb3ea103a2532ee7636012a2b1e56314c
killgriff22 <kilgrif22033@gmail.com>
2024-08-13 21:25:04 -0400

    i forgor

--- thread_variable_utility.py
+++ thread_variable_utility.py
@@ -1,7 +1,11 @@
 from patterns import *
 def read(file):
-    with open(file, 'r') as f:
-        return eval(f.read())
+        with open(file, 'r') as f:
+            try:
+                return eval(f.read())
+            except:
+                 print(f.read())
+                 return ["bounce", 0, 3, 3, 1, 1, 255, 0, 0]
 def write(file, data):
     with open(file, 'w') as f:
         f.write(str(data))
