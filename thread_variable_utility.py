8827bbb675a2e15fd67ce840625bed961e2aee68
killgriff22 <kilgrif22033@gmail.com>
2024-08-14 00:44:54 -0400

    i hope this works

--- thread_variable_utility.py
+++ thread_variable_utility.py
@@ -1,10 +1,12 @@
 from patterns import *
 def read(file):
         with open(file, 'r') as f:
+            readin = f.read()
+            f.close()
             try:
-                return eval(f.read())
+                return eval(readin)
             except:
-                 print(f.read())
+                 print(readin)
                  return ["bounce", 0, 3, 3, 1, 1, 255, 0, 0]
 def write(file, data):
     with open(file, 'w') as f:
