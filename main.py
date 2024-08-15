10855f42bc0000c1d5711f5b972e7ce50bd63f7c
killgriff22 <kilgrif22033@gmail.com>
2024-08-14 20:12:18 -0400

    its just rainbow now

--- main.py
+++ main.py
@@ -75,9 +75,9 @@ def loop():
             case "chase":
                 # take the remainder of the division of the current pixel position by num_pixels so we dont go out of bounds of the list
                 pattern[1] %= num_pixels
-            case "rainbow_cycle":
+            case "rainbow":
                 # take the remainder of the division of the current pixel position by 256 so that we dont mess up the rainbow math
-                pattern[1] = pattern[1] % 256
+                pattern[1] %= 256
             case "bounce":
                 if pattern[1] > num_pixels-1 or pattern[1] < 0:
                     pattern[5] *= -1
